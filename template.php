06aad17eef236a6a9b19474ee0aaaba4d827d588
Nigel Barink <nigelbarink@hotmail.com>
2019-11-12 14:21:24 +0100

    basic bootstrap added

--- template.php
+++ template.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" type="text/css" href="public/vendor/bootstrap/bootstrap.min.css">
+    <script src="public/vendor/bootstrap/js/bootstrap.min.js"></script>
+
     <title>WorldWideImporters Europa</title>
 </head>
 <body>
