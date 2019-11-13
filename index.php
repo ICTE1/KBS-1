7aa4b3a8948fb6da7e255d62e8d6cec0ad83da28
Nigel Barink <nigelbarink@hotmail.com>
2019-11-13 10:53:17 +0100

    fix vendor css and js, correctly including views

--- index.php
+++ index.php
@@ -3,5 +3,4 @@ require_once "inc/database.php";
 
 
 $view = "views/home.php";
-
-include "views/template.php";
+include "template.php";
--- template.php
+++ template.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" type="text/css" href="public/vendor/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="public/vendor/bootstrap/css/bootstrap.min.css">
     <script src="public/vendor/bootstrap/js/bootstrap.min.js"></script>
 
     <title>WorldWideImporters Europa</title>
