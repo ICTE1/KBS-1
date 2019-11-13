f77fd2c40f7802f924b212b481bd1f8b1e9242a4
Nigel Barink <nigelbarink@hotmail.com>
2019-11-13 11:51:47 +0100

    added a template for the json config, added a second database class for the database inbetween our sit and the wwi database

--- inc/database.php
+++ inc/database.php
@@ -1,9 +1,6 @@
 <?php
-
-
-
-class World_wide_importers_db  {
-    public $connectie ;
+class wwi_db  {
+    public $connectie = null  ;
 
     function __construct() {
         $config = json_decode(file_get_contents("/conf/db.json"));
@@ -20,10 +17,33 @@ class World_wide_importers_db  {
             echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
             exit;
         }
-        ;
+        
 
     }
     function __destruct(){
         mysqli_close($this->connectie);
+    }
+}
+
+class wwic_db {
+    public $connectie = null;
+
+    function __construct (){
+        $config = json_decode(file_get_contents("/conf/db.json"));
+
+        $host = $config->world_wide_importers_custom->host;
+        $user = $config->world_wide_importers_custom->user;
+        $pass = $config->world_wide_importers_custom->pass;
+        $dbName = $config->world_wide_importers_custom->dbName;
+
+        if (!$this->connectie =  mysqli_connect ( $host, $user , $pass , $dbName)) {
+            echo "Error: Unable to connect to the database." . PHP_EOL;
+            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+            exit;
+        }
+        
+
+
     }
 }
