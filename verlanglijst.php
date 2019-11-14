04b97e8a32bc465e7134c43ca3c9b431b33807ec
Jelluhtjuh <jellewie2002@gmail.com>
2019-11-14 12:03:13 +0100

    added the wishlist

---
+++ verlanglijst.php
@@ -0,0 +1,6 @@
+<?php
+require_once "inc/database.php";
+
+
+$view = "views/verlanglijst.php";
+include "template.php";
---
+++ views/verlanglijst.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4" style="text-align: center">
+            <h1>Verlanglijst</h1>
+        </div>
+        <div class="col-4"></div>
+    </div>
+    <div class="row verlanglijst_card">
+        <div class="col-4">
+            <p><h2>Product</h2>Prijs</p>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4">
+
+        </div>
+    </div>
+</div>
