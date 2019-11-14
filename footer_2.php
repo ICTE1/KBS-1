5b7d602f4c28c401b5a2d4964df290c52eaf99c5
Robintermeer <s1129519@student.windesheim.nl>
2019-11-14 13:15:16 +0100

    begin footer

---
+++ footer_2.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="footer_2_style.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<h2>Fixed/Sticky Footer Example</h2>
+<p>The footer is placed at the bottom of the page.</p>
+
+<div class="footer">
+    <ul class="logo_image"> <img class="WorldWideImporters" src="http://www.surroundtech.com/media/a8/ae2e/a8ae2eb1fefdbf650518e8a689f41956/wide-world-importers-logo-small.png" </ul>
+    <p>© ICTM1e1 ©</p>
+</div>
+
+</body>
+</html>
