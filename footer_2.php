4fcfe4958074b61a5305d9c15464ef1bc414e586
Robintermeer <s1129519@student.windesheim.nl>
2019-11-15 11:53:04 +0100

    kinda final footer en css bestanden

--- footer_2.php
+++ footer_2.php
@@ -4,6 +4,12 @@
 <head>
     <link rel="stylesheet" href="footer_2_style.css" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+        img{
+            padding-top: 10px;
+        }
+    </style>
 </head>
 <body>
 
@@ -11,7 +17,7 @@
 <p>The footer is placed at the bottom of the page.</p>
 
 <div class="footer">
-    <ul class="logo_image"> <img class="WorldWideImporters" src="http://www.surroundtech.com/media/a8/ae2e/a8ae2eb1fefdbf650518e8a689f41956/wide-world-importers-logo-small.png" </ul>
+    <img class="WorldWideImporters" alt="logo" src="wide-world-importers-logo-small.png"> <!--src="http://www.surroundtech.com/media/a8/ae2e/a8ae2eb1fefdbf650518e8a689f41956/wide-world-importers-logo-small.png"-->
     <p>© ICTM1e1 ©</p>
 </div>
 
