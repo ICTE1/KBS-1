5d9eef931f94ed36fa1395d8f91d33106e52ba9b
Robintermeer <s1129519@student.windesheim.nl>
2019-11-15 15:38:14 +0100

    start header

---
+++ views/basis/header.php
@@ -0,0 +1,51 @@
+<?php
+?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../../public/css/header_style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <img class="WorldWideImporters" alt="logo" src="../../public/images/wide-world-importers-logo-small.png">
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categorieën
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline my-2 ">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="loginbutton" href="login.php">login <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+</body>
+</html>
