1bc04adde7ef683eaa6c42d4e3f06efc75deda5d
Jelluhtjuh <jellewie2002@gmail.com>
2019-11-15 17:27:03 +0100

    Cleaned up styles

--- views/verlanglijst.php
+++ views/verlanglijst.php
@@ -7,74 +7,23 @@
         </div>
         <div class="col-4"></div>
     </div>
-    <div class="row verlanglijst_card">
-        <div class="col-2" style="overflow: ">
+
+    /*<div class="row verlanglijst_card">
+        <div class="col-2">
             <img class="img-fluid productThumbnail" src="public/images/space 2.jpg">
         </div>
         <div class="col-5">
-            <div style="display: inline-block; vertical-align: top; padding-left: 5px; height: 100%">
+            <div class="verlanglijst_text">
                 <p>
                     <b>Productnaam</b><br>
 
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Nam imperdiet tellus ut enim venenatis, eu euismod orci dapibus. Donec tempor.
                 </p>
-                <span style="position: absolute; bottom: 10px"><b>Prijs</b></span>
+                <span class="verlanglijst_prijs"><b>Prijs</b></span>
             </div>
         </div>
-        <div class="col-5" style="padding-top: 51px; padding-bottom: 51px; text-align: right;">
-            <form class="form-inline" style="float: right">
-                <input class="form-control" type="number" value="1" name="aantal">
-                <div class="btn btn-primary"><i class="fa fa-cart-arrow-down"></i></div>
-                <input type="hidden" name="hiddenToevoegen" value="$product_ID">
-                <div class="btn btn-primary" style="margin-left: 10px"><i class="fa fa-trash-o"></i></div>
-            </form>
-
-        </div>
-    </div>
-
-    <div class="row verlanglijst_card">
-        <div class="col-2" style="overflow: ">
-            <img class="img-fluid productThumbnail" src="public/images/space 2.jpg">
-        </div>
-        <div class="col-5">
-            <div style="display: inline-block; vertical-align: top; padding-left: 5px; height: 100%">
-                <p>
-                    <b>Productnaam</b><br>
-
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nam imperdiet tellus ut enim venenatis, eu euismod orci dapibus. Donec tempor.
-                </p>
-                <span style="position: absolute; bottom: 10px"><b>Prijs</b></span>
-            </div>
-        </div>
-        <div class="col-5" style="padding-top: 51px; padding-bottom: 51px; text-align: right;">
-            <form class="form-inline" style="float: right">
-                <input class="form-control" type="number" value="1" name="aantal">
-                <div class="btn btn-primary"><i class="fa fa-cart-arrow-down"></i></div>
-                <input type="hidden" name="hiddenToevoegen" value="$product_ID">
-                <div class="btn btn-primary" style="margin-left: 10px"><i class="fa fa-trash-o"></i></div>
-            </form>
-
-        </div>
-    </div>
-
-    <div class="row verlanglijst_card">
-        <div class="col-2" style="overflow: ">
-            <img class="img-fluid productThumbnail" src="public/images/space 2.jpg">
-        </div>
-        <div class="col-5">
-            <div style="display: inline-block; vertical-align: top; padding-left: 5px; height: 100%">
-                <p>
-                    <b>Productnaam</b><br>
-
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nam imperdiet tellus ut enim venenatis, eu euismod orci dapibus. Donec tempor.
-                </p>
-                <span style="position: absolute; bottom: 10px"><b>Prijs</b></span>
-            </div>
-        </div>
-        <div class="col-5" style="padding-top: 51px; padding-bottom: 51px; text-align: right;">
+        <div class="col-5 verlanglijst_buttons">
             <form class="form-inline" style="float: right">
                 <input class="form-control" type="number" value="1" name="aantal">
                 <div class="btn btn-primary"><i class="fa fa-cart-arrow-down"></i></div>
