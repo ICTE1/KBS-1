dac586e692ca05c2c2e99608c4f71b3d90c7d28c
Jelluhtjuh <jellewie2002@gmail.com>
2019-11-15 13:46:18 +0100

    Finished front end (for now)

--- views/verlanglijst.php
+++ views/verlanglijst.php
@@ -8,20 +8,80 @@
         <div class="col-4"></div>
     </div>
     <div class="row verlanglijst_card">
-        <div class="col-5">
+        <div class="col-2" style="overflow: ">
             <img class="img-fluid productThumbnail" src="public/images/space 2.jpg">
-            <div style="display: inline-block; vertical-align: top; padding-left: 5px;">
-                <p><b>Productnaam</b><br>Prijs</p>
+        </div>
+        <div class="col-5">
+            <div style="display: inline-block; vertical-align: top; padding-left: 5px; height: 100%">
+                <p>
+                    <b>Productnaam</b><br>
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam imperdiet tellus ut enim venenatis, eu euismod orci dapibus. Donec tempor.
+                </p>
+                <span style="position: absolute; bottom: 10px"><b>Prijs</b></span>
             </div>
         </div>
-        <div class="col-2"></div>
-        <div class="col-5" style="padding-top: 51px; padding-bottom: 51px;">
-            <form class="form-inline" style="vertical-align: center">
-                <div class="btn btn-primary">Winkelwagen</div>
+        <div class="col-5" style="padding-top: 51px; padding-bottom: 51px; text-align: right;">
+            <form class="form-inline" style="float: right">
                 <input class="form-control" type="number" value="1" name="aantal">
+                <div class="btn btn-primary"><i class="fa fa-cart-arrow-down"></i></div>
                 <input type="hidden" name="hiddenToevoegen" value="$product_ID">
+                <div class="btn btn-primary" style="margin-left: 10px"><i class="fa fa-trash-o"></i></div>
             </form>
-            <div class="fa-trash-o"></div>
+
+        </div>
+    </div>
+
+    <div class="row verlanglijst_card">
+        <div class="col-2" style="overflow: ">
+            <img class="img-fluid productThumbnail" src="public/images/space 2.jpg">
+        </div>
+        <div class="col-5">
+            <div style="display: inline-block; vertical-align: top; padding-left: 5px; height: 100%">
+                <p>
+                    <b>Productnaam</b><br>
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam imperdiet tellus ut enim venenatis, eu euismod orci dapibus. Donec tempor.
+                </p>
+                <span style="position: absolute; bottom: 10px"><b>Prijs</b></span>
+            </div>
+        </div>
+        <div class="col-5" style="padding-top: 51px; padding-bottom: 51px; text-align: right;">
+            <form class="form-inline" style="float: right">
+                <input class="form-control" type="number" value="1" name="aantal">
+                <div class="btn btn-primary"><i class="fa fa-cart-arrow-down"></i></div>
+                <input type="hidden" name="hiddenToevoegen" value="$product_ID">
+                <div class="btn btn-primary" style="margin-left: 10px"><i class="fa fa-trash-o"></i></div>
+            </form>
+
+        </div>
+    </div>
+
+    <div class="row verlanglijst_card">
+        <div class="col-2" style="overflow: ">
+            <img class="img-fluid productThumbnail" src="public/images/space 2.jpg">
+        </div>
+        <div class="col-5">
+            <div style="display: inline-block; vertical-align: top; padding-left: 5px; height: 100%">
+                <p>
+                    <b>Productnaam</b><br>
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam imperdiet tellus ut enim venenatis, eu euismod orci dapibus. Donec tempor.
+                </p>
+                <span style="position: absolute; bottom: 10px"><b>Prijs</b></span>
+            </div>
+        </div>
+        <div class="col-5" style="padding-top: 51px; padding-bottom: 51px; text-align: right;">
+            <form class="form-inline" style="float: right">
+                <input class="form-control" type="number" value="1" name="aantal">
+                <div class="btn btn-primary"><i class="fa fa-cart-arrow-down"></i></div>
+                <input type="hidden" name="hiddenToevoegen" value="$product_ID">
+                <div class="btn btn-primary" style="margin-left: 10px"><i class="fa fa-trash-o"></i></div>
+            </form>
+
         </div>
     </div>
 
