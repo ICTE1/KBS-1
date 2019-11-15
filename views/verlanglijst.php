df11f418d0bda74042c0633ef506e624d6d2ca05
Jelluhtjuh <jellewie2002@gmail.com>
2019-11-15 11:22:05 +0100

    added more elements and styling

--- views/verlanglijst.php
+++ views/verlanglijst.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container verlanglijst" style="margin-top: 10px">
     <div class="row">
         <div class="col-4"></div>
         <div class="col-4" style="text-align: center">
@@ -9,11 +9,17 @@
     <div class="row verlanglijst_card">
         <div class="col-5">
             <img class="img-fluid productThumbnail" src="public/images/space 2.jpg">
-            <p style="display: inline; vertical-align: ">Prijs</p>
+            <div style="display: inline-block; vertical-align: top; padding-left: 5px;">
+                <p><b>Productnaam</b><br>Prijs</p>
+            </div>
         </div>
-        <div class="col-3"></div>
-        <div class="col-4">
-
+        <div class="col-2"></div>
+        <div class="col-5" style="padding-top: 51px; padding-bottom: 51px;">
+            <form class="form-inline" style="vertical-align: center">
+                <div class="btn btn-primary">Winkelwagen</div>
+                <input class="form-control" type="number" value="1" id="example-number-input">
+            </form>
         </div>
     </div>
+
 </div>
