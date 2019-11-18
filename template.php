1e63dcef2525cf3fa31e358cf80142b1f7f1bd2e
Robintermeer <s1129519@student.windesheim.nl>
2019-11-18 13:28:39 +0100

    footer is compacter geworden, styling van de footer zit nu volledig in het css bestand en de footer.php en footer_2_style.css zijn nu goed gelinkt in de template

--- template.php
+++ template.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="public/vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="public/vendor/bootstrap/js/bootstrap.min.js"></script>
-
+    <link rel="stylesheet" type="text/css" href="public/css/footer_style.css">
     <title>WorldWideImporters Europa</title>
 </head>
 <body>
