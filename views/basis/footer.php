d469049a88df17feb66cf04f7b1c78c6ff155a08
Nigel Barink <nigelbarink@hotmail.com>
2019-11-19 14:03:09 +0100

    new colours :)

--- views/basis/footer.php
+++ views/basis/footer.php
@@ -2,6 +2,6 @@
 
 <div class="footer">
     <img class="WorldWideImporters" alt="logo" src="../../public/images/wide-world-importers-logo-small.png"> <!--src="http://www.surroundtech.com/media/a8/ae2e/a8ae2eb1fefdbf650518e8a689f41956/wide-world-importers-logo-small.png"-->
-    <p>© ICTM1e1 ©</p>
+    <p>© ICTM1e1</p>
 </div>
 
