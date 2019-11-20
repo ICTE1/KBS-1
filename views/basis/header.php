670d8cce42b23867fa96e2a3f6454c1b03fb2373
Robintermeer <s1129519@student.windesheim.nl>
2019-11-20 11:28:20 +0100

    Adjusted the nav-bar to have correct hover behaviour for the shoppingcart and wishlist icons

--- views/basis/header.php
+++ views/basis/header.php
@@ -1,9 +1,9 @@
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <img class="WorldWideImporters" alt="logo" src="../../public/images/wide-world-importers-logo-small.png" height="70" width="170">
+    <img class="WorldWideImporters navbar-brand" alt="logo" src="../../public/images/wide-world-importers-logo-small.png" height="70" width="170">
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
+            <li>
                 <div class="dropdown">
                     <button class="dropbtn">Categorieën
                         <i class="fa fa-caret-down"></i>
@@ -22,38 +22,33 @@
                         <a href="#">USB Powered</a>
                     </div>
                 </div>
-            </div>
-
             </li>
         </ul>
+    </div>
+
         <form class="form-inline my-2 search-area">
             <input class="form-control" type="search" placeholder="Bananen..." aria-label="Search">
             <button class="btn" type="submit">Zoeken</button>
         </form>
-        <ul class="navbar-nav mr-auto">
-            <li>
-                <div class="loginbutton">
-                <button type="button" class="btn btn-default">
-                    <a class="btn btn-outline-success my-2 my-sm-0" href="login.php">login <span class="sr-only">(current)</span></a>
-                </button>
-                </div>
-            </li>
-            <li>
-                <div class="verlanglijst">
-
-                <button type="button" class="btn btn-default">
-                    <a class="fa fa-heart" href="verlanglijst.php"></a>
-                </button>
-                </div>
-            </li>
-            <li>
-                <div class="winkelwagen">
-                <button type="button" class="btn btn-default btn-lg">
-                    <a class="fa fa-shopping-cart" href="winkelwagen.php"></a>
-                </button>
-                </div>
-            </li>
-        </ul>
+        <div class="navbar-collapse">
+            <ul class="navbar-nav mr-auto special-links">
+                <li>
+                    <div class="loginbutton">
+                    <button type="button" class="btn btn-default">
+                        <a class="btn btn-outline-success my-2 my-sm-0" href="login.php">login <span class="sr-only">(current)</span></a>
+                    </button>
+                    </div>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link text-center" href="verlanglijst.php"><i class="fa fa-heart"></i></a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="winkelwagen.php">
+                            <i class="fa fa-shopping-cart"></i>
+                        </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 
