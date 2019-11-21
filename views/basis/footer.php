130785d5f65b2ca9f3776d2e369cf97918b6ef43
Dylan Roubos <dylanroubos@Dylans-MacBook-Pro.local>
2019-11-21 08:55:45 +0100

    fixed image path to footer

--- views/basis/footer.php
+++ views/basis/footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
-    <img class="WorldWideImporters" alt="logo" src="../../public/images/wide-world-importers-logo-small.png"> <!--src="http://www.surroundtech.com/media/a8/ae2e/a8ae2eb1fefdbf650518e8a689f41956/wide-world-importers-logo-small.png"-->
+    <img class="WorldWideImporters" alt="logo" src="./public/images/wide-world-importers-logo-small.png"> <!--src="http://www.surroundtech.com/media/a8/ae2e/a8ae2eb1fefdbf650518e8a689f41956/wide-world-importers-logo-small.png"-->
     <p>© ICTM1e1 ©</p>
 </div>
