aa4100e7c8889e118995527100ff4bb1966d75be
Dylan Roubos <dylanroubos@Dylans-MacBook-Pro.local>
2019-12-01 21:20:24 +0100

    added function to keep username with wrong login

--- login.php
+++ login.php
@@ -11,8 +11,9 @@ $view = "views/login.php";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     //Clear error session variables
-    $_SESSION["username_err"] = NULL;
-    $_SESSION["password_err"] = NULL;
+    $_SESSION["username_err_l"] = NULL;
+    $_SESSION["password_err_l"] = NULL;
+    $_SESSION["username_l"] = NULL;
 
     // Define variables and initialize with empty values
     $username = $password = "";
@@ -23,11 +24,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION["username_err"] = "Vul een gebruikersnaam in";
     } else {
         $username = trim($_POST["username"]);
+        $_SESSION["username_l"] = trim($_POST["username"]);
     }
 
     // Check if password is empty
     if (empty(trim($_POST["password"]))) {
-        $_SESSION["password_err"] = "Vul je wachtwoord in";
+        $_SESSION["password_err_l"] = "Vul je wachtwoord in";
     } else {
         $password = trim($_POST["password"]);
     }
@@ -43,11 +45,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // Check if the user is known
         if (empty($user_data)) {
-            $_SESSION["username_err"] = "Wachtwoord en gebruikersnaam combinatie fout";
-            $_SESSION["password_err"] = "Wachtwoord en gebruikersnaam combinatie fout";
+            $_SESSION["username_err_l"] = "Wachtwoord en gebruikersnaam combinatie fout";
+            $_SESSION["password_err_l"] = "Wachtwoord en gebruikersnaam combinatie fout";
         }else {
             if($user_data["activated"] === 0) {
-                $_SESSION["username_err"] = "Account niet geactiveerd";
+                $_SESSION["username_err_l"] = "Account niet geactiveerd";
             }
             // Check if the filed password matches the password in the db
             elseif (password_verify($password, $user_data["password"])) {
@@ -61,8 +63,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 header("location: ./index.php");
 
             } else {
-                $_SESSION["username_err"] = "Wachtwoord en gebruikersnaam combinatie fout";
-                $_SESSION["password_err"] = "Wachtwoord en gebruikersnaam combinatie fout";
+                $_SESSION["username_err_l"] = "Wachtwoord en gebruikersnaam combinatie fout";
+                $_SESSION["password_err_l"] = "Wachtwoord en gebruikersnaam combinatie fout";
             }
         }
     }
--- views/login.php
+++ views/login.php
@@ -13,13 +13,13 @@
             <form action="login.php" method="POST">
                 <div class="form-group">
                     <label class="">Naam:</label>
-                    <input type="text" name="username" class="form-control <?php echo (isset($_SESSION['username_err'])) ? 'is-invalid' : ''; ?>">
-                    <span class="help-block"><?php if(isset($_SESSION['username_err'])) { echo $_SESSION['username_err']; } ?></span>
+                    <input type="text" name="username" <?php if (isset($_SESSION["username_l"])) { echo "value=" . $_SESSION["username_l"] . ""; }?> class="form-control  <?php echo (isset($_SESSION['username_err_l'])) ? 'is-invalid' : ''; ?>">
+                    <span class="help-block"><?php if(isset($_SESSION['username_err_l'])) { echo $_SESSION['username_err_l']; } ?></span>
                 </div>
                 <div class="form-group ">
                     <label class="">Wachtwoord:</label>
-                    <input type="password" name="password" class="form-control <?php echo (isset($_SESSION['password_err'])) ? 'is-invalid' : ''; ?>">
-                    <span class="help-block"><?php if(isset($_SESSION['password_err'])) { echo $_SESSION['password_err']; } ?></span>
+                    <input type="password" name="password" class="form-control <?php echo (isset($_SESSION['password_err_l'])) ? 'is-invalid' : ''; ?>">
+                    <span class="help-block"><?php if(isset($_SESSION['password_err_l'])) { echo $_SESSION['password_err_l']; } ?></span>
                 </div>
                 <div class="form-group">
                     <input type="submit" class="btn custom-button-primary" value="Login">
