89d67c7fd2959631b230a31242dc5e9ffd3a8138
Jelluhtjuh <jellewie2002@gmail.com>
2019-12-04 15:55:30 +0100

    amount bugfixes

--- inc/package.php
+++ inc/package.php
@@ -2,6 +2,9 @@
 function addToCart($item, $aantal){
     if(isset($_SESSION["winkelWagen"][$item])){
         $_SESSION["winkelWagen"][$item] += $aantal;
+        if($_SESSION["winkelWagen"][$item] > 1000){
+            $_SESSION["winkelWagen"][$item] = 1000;
+        }
     }
     else {
         $_SESSION["winkelWagen"][$item] = $aantal;
--- public/javascript/functions.js
+++ public/javascript/functions.js
@@ -10,11 +10,12 @@ function submitToPage(form, page){
 function updateAmount(element){
     var parent = element.parentNode;
     var aantal = element.value;
+    if(aantal > 1000){
+        aantal = 1000;
+    }
     var id = parent.getAttribute("id");
     var product_id = id.replace( /^\D+/g, '');
     var hidden = document.getElementById(product_id);
-    console.log("product: " + product_id + "\nhidden: " + hidden);
     hidden.value = aantal;
-    console.log("product: " + product_id + "\nparent_id: " + id);
 }
 
