22b335e5fcc6389366a32590cf3947f7ee6e8fd3
Nigel Barink <nigelbarink@hotmail.com>
2019-12-10 13:51:49 +0100

    No longer giving two messages saying the same thing when there are no results. Sorting controls no longer show up when there are no results

--- views/products.php
+++ views/products.php
@@ -3,18 +3,17 @@
     <?php 
         $number = count($products_to_show);
         print ("<span>{$number} resultaten</span>");
-        if ($number  <= 0 ){
-            print("<p>Geen producten</p>");
-        }
     ?>
     
-    <?php if (isset($_GET['c']) == false):?>
+    <?php if (isset($_GET['c']) == false && count ($products_to_show) > 0 ):?>
+    
     <div class='sorting-controls'>
         <span class='product-sorting-btn'>
             <a href="<?php print  (generate_sorting_link("naam") ); ?>">
             Naam <i class='fa fa-sort'></i>
             </a>
         </span>
+  
 
         <span class='product-sorting-btn'>
             <a href="<?php print (generate_sorting_link("prijs") ); ?>">
